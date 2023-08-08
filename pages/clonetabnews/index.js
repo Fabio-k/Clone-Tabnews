f553049c83dfcc2d41fd39772a805d938878b485
Fábio kazuhiro Mizo Guti <140606120+Fabio-k@users.noreply.github.com>
2023-08-08 13:31:35 +0000

    adiciona fundação do projeto

---
+++ pages/clonetabnews/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <p>projeto ainda em andamento..</p>;
+}
+
+export default Home;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Aspirante a Backend Developer</h1>;
+}
+
+export default Home;
