987d7f4cd60efd7b9b8ef33352dd61f755456b79
Fábio kazuhiro Mizo Guti <140606120+Fabio-k@users.noreply.github.com>
2023-08-09 00:53:38 +0000

    altera o index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>Aspirante a Backend Developer</h1>;
+  return (
+    <>
+      <h1>Aspirante a Backend Developer</h1>
+      <h2>Projetos</h2>
+      <li>
+        <ul>
+          <link>clone tabnews</link>
+        </ul>
+      </li>
+    </>
+  );
 }
 
 export default Home;
