da385622d15d3473afc8ed10c3be255d0d86c3ec
Fábio kazuhiro Mizo Guti <140606120+Fabio-k@users.noreply.github.com>
2023-08-12 19:32:44 +0000

    adiciona uma nova página e  o arquivo ´.editorconfig´

---
+++ pages/recomendacoes.js
@@ -0,0 +1,41 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Recomendações de canais de programação:</h1>
+      <h2>Código Fonte TV</h2>
+      <p>
+        possui playlists boas para quem está começando como o dicionário do
+        programador e dicas no geral sobre programação
+      </p>
+
+      <h2>Curso em Video</h2>
+      <p>É um canal focado em cursos que vão do iniciante ao intermediário</p>
+      <h2>Fabio Akita</h2>
+      <p>
+        é um bom canal para aprender a fundação da programação porêm é
+        necessário se esforçar para entemder o conteúdo
+      </p>
+      <h2>freecodecamp (inglês)</h2>
+      <p>possui varios tutoriais que vão do iniciante ao avançado</p>
+
+      <h2>Fireship (inglês) </h2>
+      <p>
+        possui em sua maioria videos sobre LP's e explicações sobre alguns
+        tópicos
+      </p>
+      <h2>Fiasco</h2>
+      <h2>Bolt</h2>
+
+      <h2>Lucas Montano</h2>
+
+      <h2>Filipe Dechamps</h2>
+
+      <h1>entretenimento na programação:</h1>
+      <h2>Mano Deyvin</h2>
+      <h2>Joma Tech (ingês)</h2>
+
+      <h1>Balde de agua fria do mundo da programação:</h1>
+      <h2>Curso de Tecnologia</h2>
+    </>
+  );
+}
