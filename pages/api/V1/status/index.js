f0975e8fa62c905eb2faf0148e56a02efc074afe
fabio-k <fabiokmain@gmail.com>
2024-09-25 12:21:24 -0300

    implements the endpoint `status`

--- pages/api/V1/status/index.js
+++ pages/api/V1/status/index.js
@@ -1,8 +1,27 @@
-import database from "infra/database.js";
+import query from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result.rows);
-  response.status(200).json({ chave: "resposta da api" });
+  const updatedAt = new Date().toISOString();
+  const postgreVersion = await query("SHOW SERVER_VERSION;")
+  const maxConnections = await query("SHOW MAX_CONNECTIONS;")
+  const databaseName = process.env.POSTGRES_DB
+  const openConnections = await query( {
+    text:"SELECT COUNT(*)::int used FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+
+  const databaseVersionResult = postgreVersion.rows[0].server_version
+  const databaseMaxConnectionsResult = parseInt(maxConnections.rows[0].max_connections)
+  const databaseOpenConnectionsResult = openConnections.rows[0].used
+  response.status(200).json({ 
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult,
+        max_connections: databaseMaxConnectionsResult,
+        open_connections: databaseOpenConnectionsResult
+      }
+    }
+  });
 }
 export default status;
--- test/integration/api/V1/status/get.test.js
+++ test/integration/api/V1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/V1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/V1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json()
+
+  expect(responseBody.updated_at).toBeDefined()
+  const parserdUpdatedAt = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toEqual(parserdUpdatedAt)
+
+  const database = responseBody.dependencies.database
+
+  expect(database.version).toBeDefined()
+  expect(database.max_connections).toBe(100)
+  expect(database.open_connections).toBe(1)
 });
